c936e5f023cab8aa250333fdd10ea1dc64fea298
Ivan Ferre <ivan.ferre@powercoders.org>
2023-06-11 20:55:56 +0200

    Add TODO comment.

--- pysock/echo-client.py
+++ pysock/echo-client.py
@@ -7,6 +7,9 @@ HOST = "127.0.0.1"  # server's IP address (may work also with hostname)
 PORT = 65432        # port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # TODO Catch the errors:
+    #   ConnectionRefusedError  server is not running
+    #   Connection Timed Out
     s.connect((HOST, PORT))
     s.sendall(b"Hello, World!")
     data = s.recv(1024)
