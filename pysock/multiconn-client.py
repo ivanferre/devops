0e5a1a84c405c657c12eb6b7a58e80bf410fde1f
Ivan Ferre <ivan.ferre@powercoders.org>
2023-06-12 10:26:23 +0200

    Add service_connection() to client.

--- pysock/multiconn-client.py
+++ pysock/multiconn-client.py
@@ -21,4 +21,20 @@ def start_connections (host, port, num_conns):
         data = types(SimpleNamespace, connid=connid, msg_total=sum(len(m) for m in messages), recv_total=0, messages=messages.copy(),outb=b"",)
         sel.register(sock, events, data=data)
 
-# ...
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024) # Should be ready to listen.
+        if recv_data:
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+        if not recv_data or data.recv_total == data.msg_total:
+            print(f"Closing connection {data.connid}")
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            print(f"Sending {data.outb!r} to connection {data.connid}")
+            sent = sock.data(data.outb) # Should be ready to write
+            data.outb = data.outb[sent:]
